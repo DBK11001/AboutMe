e0e6dc26f4b82b14e4d8ec06f4d812571953dc94
David Koritsanszky <david.koritsanszky@gmail.com>
2019-08-14 11:53:34 -0400

    Added a new activity_main layout

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -35,5 +35,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO (02) Connect the layout to the activity using setContentView()
+        setContentView(R.layout.activity_main)
     }
 }
